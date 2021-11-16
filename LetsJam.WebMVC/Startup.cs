a3c5176bf35cb50e9e3cc1ae166f51d39757823b
Jesse Southerland <jessemsoutherland@gmail.com>
2021-11-16 09:43:13 -0500

    Add project files.

---
+++ LetsJam.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetsJam.WebMVC.Startup))]
+namespace LetsJam.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
